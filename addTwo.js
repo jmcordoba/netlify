21c270724f7ab1b4c1bae7210b10e4ef0ef6079b
Cordoba,Juan Manuel,ES-Esplugues Llobregat <JuanManuel.Cordoba@nespresso.com>
2026-02-05 18:16:01 +0100

    first commit

---
+++ addTwo.js
@@ -0,0 +1,9 @@
+function addTwo(a, b) {
+    console.log(a + b);
+}
+
+function addThree(a, b, c) {
+    console.log(a + b + c);
+}
+
+export { addTwo, addThree };
---
+++ mathOperations.js
@@ -0,0 +1,4 @@
+import addTwo from "./addTwo";
+
+res = addTwo(1,2)
+console.log(res)
